701f879b8e688c81ad31823b6fb5f5b75151cbec
nea <neabakradze@example.com>
2020-01-12 21:36:47 +0400

    almost finidhed the header

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
+///////////////////////////SIDE BAR//////////////////////////////
 const navBurger = document.querySelector('.nav-bar');
 const closeIcon = document.querySelector('.close-bar')
 //  opening and closing the bar from the burger
 navBurger.addEventListener('click', function () {
     document.querySelector('.resp-header').classList.toggle('side-bar');
-    navBurger.src = 'glosses/close-button.svg';
-    // closeIcon.style.display = 'block';
-    // navBurger.style.display = 'none';
-
-    if (document.querySelector('.resp-header').classList.contains('side-bar') === false) {
-        navBurger.src = 'glosses/Untitled-2.svg';
-    }
+    navBurger.style.display = 'none';
 });
-// closeIcon.addEventListener('click', () => {
-//     document.querySelector('.resp-header').classList.remove('side-bar');
-//     document.querySelector('.resp-header').classList.add('unscroll');
-//     closeIcon.style.display = 'none';
-//     navBurger.style.display = 'block';
-
-// });
+document.querySelector('.second-box').addEventListener('click', function () {
+    document.querySelector('.resp-header').classList.toggle('side-bar');
+    navBurger.style.display = 'block';
+    navBurger.classList.add('fade-click');
+});
 //  closing the bar from the side bar itself
 document.querySelector('.close-button').addEventListener('click', () => {
     document.querySelector('.resp-header').classList.remove('side-bar');
+    navBurger.style.display = 'block';
+    navBurger.classList.add('display-block-none')
+});
+
+///////////////SEARCH BAR///////////////////
+document.querySelector('.search-button').addEventListener('click', () => {
+    // document.querySelector('.search-product-full-screen').style.display = 'block';
+    document.querySelector('.search-product-full-screen').classList.toggle('display-block-search-bar');
 });
