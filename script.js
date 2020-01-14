dde8eed61c33678b39bdd30eca08544d754c0b34
nea <neabakradze@example.com>
2020-01-14 15:15:28 +0400

    kinda finished the header

--- script.js
+++ script.js
@@ -23,3 +23,17 @@ document.querySelector('.search-button').addEventListener('click', () => {
     // document.querySelector('.search-product-full-screen').style.display = 'block';
     document.querySelector('.search-product-full-screen').classList.toggle('display-block-search-bar');
 });
+
+///////////product drop down bar////////////////
+document.querySelector('.drop-down').addEventListener('click', () => {
+    // document.querySelector('.search-product-full-screen').style.display = 'block';
+    document.querySelector('.dropdown-bar').classList.toggle('display');
+});
+// document.querySelector('.arrow').addEventListener('click', () => {
+//     // document.querySelector('.search-product-full-screen').style.display = 'block';
+//     document.querySelector('.dropdown-bar').classList.toggle('display');
+// });
+document.getElementById('product-drop-bar').addEventListener('click', () => {
+    document.querySelector('.resp-drop').classList.toggle('display');
+
+});
